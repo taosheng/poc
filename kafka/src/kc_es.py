4dd3c23eb00d0f8688893cae52325cbc77d8058c
root <Doug Chen>
2016-10-14 17:55:45 +0800

    insert to elasticsearch by default index

--- kafka/src/kc_es.py
+++ kafka/src/kc_es.py
@@ -3,19 +3,27 @@
 
 import os
 import sys
-
 import json
 from kafka import KafkaProducer
+from kafka import KafkaConsumer
 from kafka.errors import KafkaError
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from datetime import datetime
+import time
+
 BROKERS_LIST = ['localhost:9092'] 
+ES_HOST = '10.1.192.79'
 TOPIC = "my-topic"
 
+#es.indices.refresh(index="testi")
 
-import time
-from kafka import KafkaConsumer
-
-# To consume latest messages and auto-commit offsets
-def readKafka():
+# To consume latest messages and index in es
+def consumeKafkaToEs():
+    es = Elasticsearch(
+        hosts=[{'host': ES_HOST, 'port': 9200}],
+        connection_class=RequestsHttpConnection
+    )
+    print(es.info())
     consumer = KafkaConsumer(TOPIC,
                          group_id='my-group',
                          bootstrap_servers=['localhost:9092'],
@@ -26,6 +34,9 @@ def readKafka():
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
+        toInsert = json.loads(message.value.decode("utf-8") )
+        res = es.index(index="test-index", doc_type='fb',  body=toInsert)
+        print("done insert")
         time.sleep(1)
 
 
@@ -35,5 +46,5 @@ if __name__ == '__main__':
         exit(0)
 
     TOPIC = sys.argv[1]
-    readKafka()
+    consumeKafkaToEs()
     
