63edc059368637aacc72d409ec5b12020ab28557
chen taosheng <dougchen@talent-service.com>
2017-04-19 10:36:47 +0800

    count pushEvent as working-number, a bit refactoring

--- hunterTool/src/main.py
+++ hunterTool/src/main.py
@@ -2,41 +2,58 @@
 # -*- coding: utf-8 -*-
 from github import Github
 import sys
+import pprint
+import datetime
+
+def getUserPushEventsNo(namedUser, daysBefore):
+    events = namedUser.get_events()
+    pp = pprint.PrettyPrinter(indent=4)
+    cnt = 0
+    bef = datetime.datetime.now() - datetime.timedelta(days=daysBefore)
+    for e in events:
+        if e.type == 'PushEvent' and e.created_at > bef:
+            cnt += 1
+      #  pp.pprint(e.payload)
+    return cnt
 
 def showUserInfo(namedUser):
     info = u''
 #    info = info + namedUser.name.encode('utf-8') + " "
-    info = info + str(namedUser.email) +", "
+    info = info + str(namedUser.email) +"; "
     try:
         location = namedUser.location.decode('UTF-8')
-        info = info+'"' + location + '", '
+        info = info+'"' + location + '"; '
     except:
-        info = info +"<location encoding>, "
+        info = info +"<encoding issue>; "
 #    info = info + "cont:"+str(namedUser.contributions) + ", "
 #    info = info + "name:"+str(namedUser.name) + ", "
-    info = info + "personal_web:"+str(namedUser.blog) + ", "
-    info = info + "giturl:"+str(namedUser.html_url) + ", "
-    info = info + "prepo:"+str(namedUser.public_repos) + ", "
+    try:
+        info = info + "personal_web: "+str(namedUser.blog) + "; "
+    except:
+        info = info + "personal_web: <encoding issue>" + "; "
+    info = info + "giturl: "+str(namedUser.html_url) + "; "
+    info = info + "prepo:"+str(namedUser.public_repos) + "; "
 #    info = info + "repo:"+str(namedUser.get_repos) + ", "
 #    info = info + "watch:"+str(namedUser.get_watched) + ", "
 #    info = info + "starred:"+str(namedUser.get_starred) + ", "
-    info = info + "following:"+str(namedUser.following) + ", "
-    info = info + "follower:"+str(namedUser.followers) + ", "
+    info = info + "following:"+str(namedUser.following) + "; "
+    info = info + "follower:"+str(namedUser.followers) + "; "
 
     return info
 
 
-def showUsers(gita):
-    us = gita.search_users('language:javascript location:taiwan')
+def showUsers(gita, q):
+    us = gita.search_users(q)
     #us = gita.search_users('language:javascript',lauguage='javascript',location='taiwan')
     limit = 1
     for u in us:
-        if u.email is None:
+        if u.email is None and u.blog is None:
             continue
-        print(showUserInfo(u))
-        limit += 1
-        if limit >= 200: 
-            break
+        print(showUserInfo(u)+" work:"+ str(getUserPushEventsNo(u,365)))
+#        limit += 1
+#        if limit >= 5: 
+#            break
+#    print(limit)
 
 def showRepoByKeyword(gita, keyword):
 #    rs=gita.search_repositories(keyword,language='javascript')
@@ -76,7 +93,8 @@ if __name__ == '__main__' :
 #    password = raw_input()
     gita = Github(sys.argv[1],sys.argv[2])
     #showRepoByKeyword(gita, "node.js api")
-    showUsers(gita)
+    showUsers(gita, 'language:javascript location:taiwan')
+    #showUsers(gita, 'dougchen language:python')
 
 
 # Then play with your Github objects:
